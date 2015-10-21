40f938081dec3f3e5a496ff2c271aac5acbef953
gnishida <gnishida@purdue.edu>
2015-10-21 13:53:34 -0400

    initial commit

---
+++ CPUShader/MainWindow.cpp
@@ -0,0 +1,13 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent, Qt::WFlags flags) : QMainWindow(parent, flags) {
+	ui.setupUi(this);
+
+	glWidget = new GLWidget3D();
+	setCentralWidget(glWidget);
+}
+
+MainWindow::~MainWindow()
+{
+
+}
